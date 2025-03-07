d89aaf52db6eb80254ffc631250c8f3216046f39
pixlfigma <pixlfigma@gmail.com>
2025-03-07 08:16:53 +0530

    fix: added the google drive authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     #Google Drive
     path('google-drive-authentication', views.GoogleDriveAuthenticationView.as_view(), name='google-drive-authentication'),
     path('google-drive-callback', views.GoogleDriveCallbackView.as_view(), name='google-drive-callback'),
+    path('google-drive-upload', views.GoogleDriveUploadView.as_view(), name='google-drive-upload'),
+    path('google-drive-files-list', views.GoogleDriveListFilesView.as_view(), name='google-drive-files-list'),
+    path('google-drive-download-file', views.GoogleDriveDownloadView.as_view(), name='google-drive-download-file'),
 ]
